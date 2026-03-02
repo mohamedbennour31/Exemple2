f17c1b7b2cbc25fc0df2cb4a660261a4406f5dfd
mohamedbennour31 <100688823+mohamedbennour31@users.noreply.github.com>
2026-03-02 10:53:26 +0100

    Update StudentManagementApplicationTests.java

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,13 +1,21 @@
 package tn.esprit.studentmanagement;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class StudentManagementApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void contextLoads() {
+        // Cette assertion vérifie que le contexte Spring Boot 
+        // a démarré correctement et n'est pas nul.
+        assertThat(context).isNotNull();
     }
-
 }
